73bbe156ac07a78e53909d112ada0706f4a8c057
woals2840 <woals2840@gmail.com>
2026-01-07 01:36:18 +0900

    feat #5 - RecentViewedCollection 엔티티 생성

---
+++ modules/bookmark/src/main/java/kr/flint/bookmark/domain/RecentViewedCollection.java
@@ -0,0 +1,26 @@
+package kr.flint.bookmark.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import kr.flint.shared.domain.BaseTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RecentViewedCollection extends BaseTime {
+	@Column(nullable = false)
+	private Long collectionId;
+
+	@Column(nullable = false)
+	private Long userId;
+
+	public static RecentViewedCollection create(Long userId, Long collectionId) {
+		return new RecentViewedCollection(userId, collectionId);
+	}
+
+}
