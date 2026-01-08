c5a3988e4cda304e006876cf62ff259e2d644dc3
woals2840 <woals2840@gmail.com>
2026-01-08 17:52:32 +0900

    feat #10 - Collection 관련 레포지토리 생성

--- modules/collection/src/main/java/kr/flint/collection/dto/request/CreateCollectionReq.java
+++ modules/collection/src/main/java/kr/flint/collection/dto/request/CreateCollectionReq.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 
 public record CreateCollectionReq(
 	@NotNull(message = "컬렉션 이미지는 필수 입력값입니다")
-	String url,
+	String imageUrl,
 	@NotNull(message = "컬렉션 제목은 필수 입력 값입니다")
 	@Size(min = 0, max = 20)
 	String title,
@@ -16,6 +16,6 @@ public record CreateCollectionReq(
 	@NotNull(message = "공개여부는 필수 입력값입니다")
 	boolean isPublic,
 	@NotNull(message = "작품은 필수 입력값입니다")
-	List<Long> contentIdList
+	List<AddContentReq> contentList
 ) {
 }
