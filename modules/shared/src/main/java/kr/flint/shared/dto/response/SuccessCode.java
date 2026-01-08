aa97403ba9591183917179e0aa88ff8632a4cb00
woals2840 <woals2840@gmail.com>
2026-01-08 17:51:37 +0900

    feat #10 - Success 공통응답 구현

---
+++ modules/shared/src/main/java/kr/flint/shared/dto/response/SuccessCode.java
@@ -0,0 +1,20 @@
+package kr.flint.shared.dto.response;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessCode {    /* 201 CREATED */
+	SUCCESS_CREATE(HttpStatus.CREATED, "생성이 완료되었습니다"),
+
+	/* 200 OK */
+	SUCCESS_UPDATE(HttpStatus.OK, "업데이트가 완료되었습니다"),
+	SUCCESS_DELETE(HttpStatus.OK, "삭제가 완료되었습니다"),
+	SUCCESS_FETCH(HttpStatus.OK, "요청 데이터가 성공적으로 조회되었습니다");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
