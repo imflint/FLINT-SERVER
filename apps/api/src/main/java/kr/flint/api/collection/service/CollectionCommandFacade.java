fcdb6f46ca6647f7aac39a30287bdf7c092946ea
woals2840 <woals2840@gmail.com>
2026-01-09 19:22:30 +0900

    feat #14 - facade 계층 @Component로 어노테이션 변경

--- apps/api/src/main/java/kr/flint/api/collection/service/CollectionCommandFacade.java
+++ apps/api/src/main/java/kr/flint/api/collection/service/CollectionCommandFacade.java
@@ -1,17 +1,14 @@
 package kr.flint.api.collection.service;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.flint.collection.dto.request.CreateCollectionReq;
 import kr.flint.collection.service.CollectionService;
-import kr.flint.user.domain.User;
 import kr.flint.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Service
-@Transactional(readOnly = true)
+@Component
 @RequiredArgsConstructor
 public class CollectionCommandFacade {
 	private final CollectionService collectionService;
