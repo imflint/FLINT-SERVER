3b2e5a12f4d72fe92cbc79844afe772ed7d9011f
hojooo <ghwn5833@gmail.com>
2026-01-12 01:15:25 +0900

    refactor: dev config 추가

--- infra/storage/src/main/java/kr/flint/infra/storage/s3/config/S3ProdConfig.java
+++ infra/storage/src/main/java/kr/flint/infra/storage/s3/config/S3ProdConfig.java
@@ -11,7 +11,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-@Profile("prod")
+@Profile({"prod", "dev"})
 @EnableConfigurationProperties(S3Properties.class)
 public class S3ProdConfig {
 
