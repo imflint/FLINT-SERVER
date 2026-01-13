63fc002387a549bc02fdb6f9e4dba0697dae66fa
woals2840 <woals2840@gmail.com>
2026-01-13 23:20:47 +0900

    feat/#39 - FeignClient 추가

--- apps/api/src/main/java/kr/flint/api/FlintApiApplication.java
+++ apps/api/src/main/java/kr/flint/api/FlintApiApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "kr.flint")
 @ConfigurationPropertiesScan(basePackages = "kr.flint")
 @EnableJpaRepositories(basePackages = "kr.flint")
+@EnableFeignClients(basePackages = "kr.flint")
 @EntityScan(basePackages = "kr.flint")
 public class FlintApiApplication {
 
