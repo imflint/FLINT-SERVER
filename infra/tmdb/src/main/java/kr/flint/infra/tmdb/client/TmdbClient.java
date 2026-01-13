9d2b52461a125fac3dcca2669c0ffa897c2dcdf6
woals2840 <woals2840@gmail.com>
2026-01-13 23:21:03 +0900

    feat/#39 - TMDB 세팅

---
+++ infra/tmdb/src/main/java/kr/flint/infra/tmdb/client/TmdbClient.java
@@ -0,0 +1,51 @@
+package kr.flint.infra.tmdb.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.flint.infra.tmdb.config.TmdbFeignConfig;
+import kr.flint.infra.tmdb.dto.TmdbCommonRes;
+import kr.flint.infra.tmdb.dto.TmdbContentRes;
+import kr.flint.infra.tmdb.dto.TmdbGenreListRes;
+import kr.flint.infra.tmdb.dto.TmdbMovieCreditRes;
+import kr.flint.infra.tmdb.dto.TmdbTvDetailRes;
+
+@FeignClient(
+	name = "tmdbClient",
+	url = "${tmdb.base-url}",
+	configuration = TmdbFeignConfig.class
+)
+public interface TmdbClient {
+	@GetMapping("/3/movie/popular")
+	TmdbCommonRes getPopularMovieList(
+		@RequestParam(value = "language", defaultValue = "ko-KR") String language,
+		@RequestParam(value = "page", defaultValue = "1") int page
+	);
+
+	@GetMapping("/3/search/multi")
+	TmdbCommonRes getMultiList(
+		@RequestParam("query") String query,
+		@RequestParam(value = "language", defaultValue = "ko-KR") String language,
+		@RequestParam(value = "page", defaultValue = "1") int page
+	);
+
+	@GetMapping("/3/movie/{id}/credits")
+	TmdbMovieCreditRes getMovieCredit(
+		@PathVariable("id") Long id,
+		@RequestParam(value = "language", defaultValue = "ko-KR") String language
+	);
+
+	@GetMapping("/3/tv/{id}")
+	TmdbTvDetailRes getTvDetail(
+		@PathVariable("id") Long id,
+		@RequestParam(value = "language", defaultValue = "ko-KR") String language
+	);
+
+	@GetMapping("/3/movie/{id}")
+	TmdbGenreListRes getMovieDetail(
+		@PathVariable("id") Long id,
+		@RequestParam(value = "language", defaultValue = "ko-KR") String language
+	);
+}
