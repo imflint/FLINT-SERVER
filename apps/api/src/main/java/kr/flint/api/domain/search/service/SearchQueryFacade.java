454c3fb7dbebd28f21f7c9a4e9f1d73b05cc97f3
woals2840 <woals2840@gmail.com>
2026-01-15 03:39:01 +0900

    feat #62 - 충돌 해결 중

--- apps/api/src/main/java/kr/flint/api/domain/search/service/SearchQueryFacade.java
+++ apps/api/src/main/java/kr/flint/api/domain/search/service/SearchQueryFacade.java
@@ -53,7 +53,7 @@ public class SearchQueryFacade {
 			: null;
 
 		return PaginationResponse.ofCursor(SliceCursor.of(data, null, nextCursor));
-		}
+	}
 
 	public PaginationResponse<BookmarkedContentSearchRes> searchBookmarkedContents(
 		final Long userId,
