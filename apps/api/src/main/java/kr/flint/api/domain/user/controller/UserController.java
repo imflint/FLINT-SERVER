ad5e0f08dd9de44f46e260b4cfc08c1731309eb0
hojooo <ghwn5833@gmail.com>
2026-01-14 23:34:13 +0900

    [HOTFIX] userController interface 구현 수정 (#60)

--- apps/api/src/main/java/kr/flint/api/domain/user/controller/UserController.java
+++ apps/api/src/main/java/kr/flint/api/domain/user/controller/UserController.java
@@ -15,7 +15,6 @@ import kr.flint.shared.dto.response.SuccessResponse;
 import kr.flint.user.dto.response.NicknameCheckResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
