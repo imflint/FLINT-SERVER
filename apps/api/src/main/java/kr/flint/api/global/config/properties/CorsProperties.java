80091fe50b7e18d972424538aab1b070ce149fe2
hojooo <ghwn5833@gmail.com>
2026-01-16 01:10:28 +0900

    fix: 누락된 설정들 추가 (#73)

---
+++ apps/api/src/main/java/kr/flint/api/global/config/properties/CorsProperties.java
@@ -0,0 +1,11 @@
+package kr.flint.api.global.config.properties;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "cors")
+public record CorsProperties(
+	List<String> allowedOrigins
+) {
+}
