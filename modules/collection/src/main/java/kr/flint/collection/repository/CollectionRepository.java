ae5616fb5c224098a716c5177636ba22a1550215
woals2840 <143878241+woals2840@users.noreply.github.com>
2026-01-21 23:22:26 +0900

    Refactor/#121 (#122)

--- modules/collection/src/main/java/kr/flint/collection/repository/CollectionRepository.java
+++ modules/collection/src/main/java/kr/flint/collection/repository/CollectionRepository.java
@@ -3,6 +3,9 @@ package kr.flint.collection.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import kr.flint.collection.domain.Collection;
@@ -14,4 +17,20 @@ public interface CollectionRepository extends JpaRepository<Collection, Long> {
     List<CollectionSummaryProjection> findByUserId(Long userId);
 
     List<CollectionSummaryProjection> findByIdIn(List<Long> ids);
+
+	@Modifying
+	@Query(value = """
+		UPDATE collection
+		SET bookmark_count = bookmark_count + 1
+		WHERE id = :collectionId
+""", nativeQuery = true)
+	int incBookmarkCount(@Param("collectionId")Long collectionId);
+
+	@Modifying
+	@Query(value = """
+        UPDATE collection
+        SET bookmark_count = CASE WHEN bookmark_count > 0 THEN bookmark_count - 1 ELSE 0 END
+        WHERE id = :collectionId
+        """, nativeQuery = true)
+	int decBookmarkCount(@Param("collectionId") Long collectionId);
 }
