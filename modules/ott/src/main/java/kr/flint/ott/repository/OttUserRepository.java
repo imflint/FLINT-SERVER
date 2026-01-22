27bd504b6d8efc378679f89c101afc8549f60664
woals2840 <woals2840@gmail.com>
2026-01-23 03:06:52 +0900

    refactor/#137 - 프롬프트 재작성

--- modules/ott/src/main/java/kr/flint/ott/repository/OttUserRepository.java
+++ modules/ott/src/main/java/kr/flint/ott/repository/OttUserRepository.java
@@ -22,14 +22,15 @@ public interface OttUserRepository extends JpaRepository<OttUser, Long> {
     )
     from OttUser ou
     join ou.ottProvider op
+    join OttContent oc
+        on oc.ottProvider = op
     where ou.userId = :userId
-      and op.id in (
-          select oc.ottProvider.id
-          from OttContent oc
-          where oc.contentId = :contentId
-      )
+      and oc.contentId = :contentId
 """)
-	List<GetOttResponse> getSubscribeOttList(Long userId, Long contentId);
+	List<GetOttResponse> getSubscribeOttList(
+		@Param("userId") Long userId,
+		@Param("contentId") Long contentId
+	);
 
 	void deleteAllByUserId(Long userId);
 }
